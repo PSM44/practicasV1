6a7ccc5411899b3e603265fd3ef0eabfc4232c98
PSM44 <6psanmartin@gmail.com>
2025-08-23 12:10:21 -0400

    subida de estructura con spring Initlz

---
+++ src/test/java/com/ipss/practicas/PracticasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipss.practicas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
